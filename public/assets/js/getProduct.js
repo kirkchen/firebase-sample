31d92ba0003043a7711e3ac30769bb7c82c07984
Kirk Chen <rwk0119@yahoo.com.tw>
2019-01-26 15:18:38 +0800

    Add to cart

--- public/assets/js/getProduct.js
+++ public/assets/js/getProduct.js
@@ -1,25 +1,35 @@
 $(function() {
-  var productId = getProductId();
-
-  var productRef = firebase.database().ref("/products/" + productId);
-  productRef.on("value", function(snapshot) {
-    var product = snapshot.val();
-
-    $("#title").text(product.title);
-    $("#price").text(product.price);
-    $("#count").text(product.count);
-    $("#description").text(product.description);
-    $("#picture").attr('src', product.url);
-
-    $addToCart = $("#add-to-cart");
-    $soldout = $("#soldout");
-    $addToCart.hide();
-    $soldout.hide();
-    if (product.count > 0) {
-      $addToCart.show();
-    } else {
-      $soldout.show();
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (!user) {
+      window.location.href = "login.html";
     }
+
+    var productId = getProductId();
+
+    var productRef = firebase.database().ref("/products/" + productId);
+    productRef.on("value", function(snapshot) {
+      var product = snapshot.val();
+
+      $("#title").text(product.title);
+      $("#price").text(product.price);
+      $("#description").text(product.description);
+      $("#picture").attr("src", product.url);
+
+      $addToCart = $("#add-to-cart");
+      $soldout = $("#soldout");
+      $addToCart.hide();
+      $soldout.hide();
+      if (product.count > 0) {
+        $addToCart.show();
+      } else {
+        $soldout.show();
+      }
+
+      $(document).on("click", "#add-to-cart", function() {
+        var count = $('#count').val();
+        addProductIdToShoppingCart(user.uid, productId, count)
+      });
+    });
   });
 });
 
@@ -28,4 +38,11 @@ function getProductId() {
   var prodcutId = params.get("id");
 
   return prodcutId;
+}
+
+function addProductIdToShoppingCart(userId, productId, count) {
+  var shoppingCartRef = firebase.database().ref("shoppingCart");
+  var shoppingCartItemRef = shoppingCartRef.child(userId).child(productId);
+
+  shoppingCartItemRef.set(count);
 }
