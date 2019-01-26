ed4ee2808ef8c93f67be01dfed816bea32fbda2b
Kirk Chen <rwk0119@yahoo.com.tw>
2019-01-26 11:32:15 +0800

    Add display product

---
+++ public/assets/js/getProduct.js
@@ -0,0 +1,30 @@
+$(function() {
+  var productId = getProductId();
+
+  var productRef = firebase.database().ref("/products/" + productId);
+  productRef.on("value", function(snapshot) {
+    var product = snapshot.val();
+
+    $("#title").text(product.title);
+    $("#price").text(product.price);
+    $("#count").text(product.count);
+    $("#description").text(product.description);
+
+    $addToCart = $("#add-to-cart");
+    $soldout = $("#soldout");
+    $addToCart.hide();
+    $soldout.hide();
+    if (product.count > 0) {
+      $addToCart.show();
+    } else {
+      $soldout.show();
+    }
+  });
+});
+
+function getProductId() {
+  var params = new URLSearchParams(window.location.search);
+  var prodcutId = params.get("id");
+
+  return prodcutId;
+}
